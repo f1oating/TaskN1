4474fe956534185cdcaf79aea1c7bd572f1f14e7
f1oating <dtoronchenko@gmail.com>
2024-01-11 16:31:08 +0200

    Added css and refactored validation classes (an problem occured during last commit)

--- src/main/java/by/toronchenko/taskn1/validators/CompanyValidator.java
+++ src/main/java/by/toronchenko/taskn1/validators/CompanyValidator.java
@@ -2,10 +2,16 @@ package by.toronchenko.taskn1.validators;
 
 import by.toronchenko.taskn1.entity.Company;
 import by.toronchenko.taskn1.entity.User;
+import by.toronchenko.taskn1.repositories.CompanyCrudRepository;
+import by.toronchenko.taskn1.repositories.UserCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompanyValidator implements Validator<Company>{
+
+    @Autowired
+    private CompanyCrudRepository companyCrudRepository;
 
     @Override
     public ValidationResult isValid(Company company) {
@@ -13,7 +19,19 @@ public class CompanyValidator implements Validator<Company>{
         if(company.getName().isEmpty()){
             validationResult.add(Error.of("invalid.name", "Name is empty!"));
         }
+        if(company.getName().length() > 16){
+            validationResult.add(Error.of("invalid.name", "Name to long!"));
+        }
+        if(isExist(company)){
+            validationResult.add(Error.of("database.exists", "Company already exists!"));
+        }
         return validationResult;
+    }
+
+    private boolean isExist(Company company){
+        return companyCrudRepository.findAll().stream().anyMatch(c ->
+                (c.getName().equals(company.getName())) && (c.getCompany_id() != company.getCompany_id())
+        );
     }
 
 }
